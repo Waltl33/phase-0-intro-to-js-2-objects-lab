32b7bacb1138dbb07684b755065a5eb3e2158708
Walter <walter.clark24@yahoo.com>
2023-04-13 11:58:24 -0400

    commit tree cdcfbef52e6c1cfc479b126d70b6794c5301fce1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: "Bill",
+    streetAddress: "yep"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, ...{[key]: value}}
+}
